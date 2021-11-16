6c1c0f2153ac17718c81812457df58586d6e9e95
Cyan <huetcyan@gmail.com>
2021-11-16 22:59:39 +0100

    Ajout des fonctions relatives aux playlists
    - Création d'une playlist
    - Recherche par idPlaylist
    - Recherche par idUtilisateur
    - Mise à jour du titre et/ou des idVideos d'une playlist
    - Suppression d'une playlist

--- api/app.js
+++ api/app.js
@@ -8,20 +8,22 @@ const { Playlist, Annonce } = require('./db/models')
 
 app.use(express.json())
 
+// Récupération de toutes les playlists
 app.get('/playlists', (req, res) => {
-
     Playlist.find({}).then((playlists) => {
         res.send(playlists)
     })
 })
 
-
+// Création d'une playlist. 
 app.post('/playlists', (req, res) => {
     let titre = req.body.titre
     let idVideos = req.body.idVideos
+    let idUtilisateur = req.body.idUtilisateur
     let newPlaylist = new Playlist({
         titre,
-        idVideos
+        idVideos,
+        idUtilisateur
     })
     newPlaylist.save().then((PlaylistDoc) => {
         res.send(PlaylistDoc)
@@ -29,14 +31,44 @@ app.post('/playlists', (req, res) => {
 
 })
 
-app.patch('/playlists/:id', (req, res) => {
-    Playlist.findOneAndUpdate({ _id: req.params.id }, {
-        $set: req.body
-    }).then(() => {
-        res.sendStatus(200);
+// Récupération de toutes les playlists d'un utilisateur par son identifiant.
+app.get('/playlistsFromUser/:id', (req, res) => {
+    Playlist.find({ idUtilisateur: req.params.id }).then((playlists) => {
+        res.send(playlists)
     })
 })
 
+// Récupération de la playlist par l'identifiant de la playlist.
+app.get('/playlists/:id', (req, res) => {
+    Playlist.findOne({ _id: req.params.id }).then((playlists) => {
+        res.send(playlists)
+    })
+})
+
+
+// Ajout ou suppression d'un identifiant de vidéo à la playlist avec les champs "edit" et "del".
+// Modification du titre de la playlist avec le champ "titre".
+app.patch('/playlists/:id', (req, res) => {
+    if (req.body.action == "edit") {
+        Playlist.findOneAndUpdate({ _id: req.params.id }, {
+            $addToSet: {
+                idVideos: req.body.idVideo,
+            },
+            titre: req.body.titre
+        }).then(() => {
+            res.sendStatus(200);
+        })
+    } else if (req.body.action == "del") {
+        Playlist.findOneAndUpdate({ _id: req.params.id }, {
+            $pull: { idVideos: req.body.idVideo }
+        }).then(() => {
+            res.sendStatus(200);
+        })
+    }
+})
+
+
+// Suppression d'une playlist par son identifiant.
 app.delete('/playlists/:id', (req, res) => {
     Playlist.findOneAndRemove({
         _id: req.params.id
@@ -44,6 +76,7 @@ app.delete('/playlists/:id', (req, res) => {
         res.send(removedPlaylistDoc)
     })
 })
+
 
 
 app.get('/annonces')
