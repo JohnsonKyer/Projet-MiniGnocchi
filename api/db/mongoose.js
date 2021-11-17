c6213333f4635a4f37da3d07b95009c054fabc31
FlofloMAC <ibanez.florent@gmail.com>
2021-11-17 16:33:43 +0100

    mangodb base

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -3,12 +3,15 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/MiniGnocchi', { useNewUrlParser: true }).then(() => {
+const uri = "mongodb+srv://MiniGnocchi:minibaby@minicluster.metfo.mongodb.net/MiniGloabal?retryWrites=true&w=majority";
+mongoose.connect(uri, { useNewUrlParser: true }).then(() => {
     console.log("Connexion effectuée à la base ! ;)")
 }).catch((e) => {
     console.log("Erreur lors de la connexion à la BDD.");
     console.log(e);
 })
+
+
 module.exports = {
     mongoose
 };
