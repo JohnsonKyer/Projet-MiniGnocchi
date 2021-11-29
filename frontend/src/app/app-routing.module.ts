e1b512b571d6dbc4e9d77dce7330957fea5f8695
FlofloMAC <ibanez.florent@gmail.com>
2021-11-29 11:46:20 +0100

    authguard home

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,23 +4,24 @@ import { LoginComponent } from './login/login.component';
 import { PlaylistsComponent } from './pages/playlists/playlists.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthGuard } from './services/auth.guard';
-import {HomeComponent} from "./home/home.component";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
   {
-    path: '', canActivate:[AuthGuard],
-    component:PlaylistsComponent
+    path: '', canActivate: [AuthGuard],
+    component: PlaylistsComponent
   },
   {
-    path:'register',
-    component:RegisterComponent
-  },
-  {path:'login',
-    component:LoginComponent
+    path: 'register',
+    component: RegisterComponent
   },
   {
-    path:'home',
-    component:HomeComponent
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home', canActivate: [AuthGuard],
+    component: HomeComponent
   }
 ];
 
