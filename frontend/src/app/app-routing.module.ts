fdd582da1354fb55243df70fad603c7e569bdbe1
Cyan <huetcyan@gmail.com>
2021-11-29 23:09:24 +0100

    Profil Utilisateur component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AuthGuard } from './services/auth.guard';
 import { HomeComponent } from "./home/home.component";
 import {PlaylistComponent} from "./playlist/playlist.component";
 import {VideoPlaylistComponent} from "./video-playlist/video-playlist.component";
+import { ProfilUtilisateurComponent } from './profil-utilisateur/profil-utilisateur.component';
 
 const routes: Routes = [
   {
@@ -25,7 +26,9 @@ const routes: Routes = [
   },
   { path: 'videoPlaylist', canActivate: [AuthGuard],
     component: VideoPlaylistComponent
-  }
+  },
+  {path: 'profilUtilisateur', canActivate: [AuthGuard],
+  component: ProfilUtilisateurComponent}
 ];
 
 @NgModule({
