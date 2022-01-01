2203314a2f34a0310dd3a25f6f1bf16ba3868989
Cyan <huetcyan@gmail.com>
2022-01-02 00:00:28 +0100

    call api pour annonce aléatoire, function pour incrémenter les vues d'une annonce

--- api/app.js
+++ api/app.js
@@ -7,7 +7,6 @@ const controller = require("./controllers/auth.controller");
 const {authJwt} = require("./middlewares");
 var bcrypt = require("bcryptjs");
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
 
 
 const {mongoose} = require('./db/mongoose')
@@ -138,7 +137,25 @@ app.get('/annonceur/annonces/:id', async (req, res) => {
         res.send(annonceur[0].annonces)
     })
 
-})
+});
+
+app.get('/annonceur/annoncealeatoire', async (req, res) => {
+    Annonceur.count({suspendu: undefined}).then((count) => {
+        let random = Math.floor(Math.random() * count);
+        Annonceur.find({suspendu: undefined}).skip(random).then((annonceur) => {
+            let random = Math.floor(Math.random() * annonceur[0].annonces.length);
+            res.send(annonceur[0].annonces[random]);
+            addImpression(annonceur[0].annonces[random]._id)
+        })
+    })
+});
+
+
+function addImpression(id) {
+    Annonceur.findOneAndUpdate({'annonces._id': id},
+        {$inc: {'annonces.$.impressions': 1}});
+}
+
 const ImgurStorage = require('multer-storage-imgur');
 const multer = require('multer');
 const upload = multer({
