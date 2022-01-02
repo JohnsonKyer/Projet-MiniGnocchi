085e2d396f7a95c193596156d3703ee8d72fdd6e
Cyan <huetcyan@gmail.com>
2022-01-02 05:05:08 +0100

    Popup annonce sur les vidéos
    1 chance sur 3 d'afficher une annonce

--- frontend/src/app/watch-video-playlist/watch-video-playlist.component.ts
+++ frontend/src/app/watch-video-playlist/watch-video-playlist.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { ActivatedRoute } from '@angular/router';
-import { HttpClient } from "@angular/common/http";
-import { FlashMessagesService } from "angular2-flash-messages";
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {DomSanitizer, SafeResourceUrl} from '@angular/platform-browser';
+import {ActivatedRoute} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {FlashMessagesService} from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-watch-video-playlist',
@@ -10,38 +10,45 @@ import { FlashMessagesService } from "angular2-flash-messages";
   styleUrls: ['./watch-video-playlist.component.scss']
 })
 export class WatchVideoPlaylistComponent implements OnInit {
+  @ViewChild('buttonpopup') buttonpopup: ElementRef;
+
   id: string;
   title: string;
   link: string;
   idPlaylist: string;
   urlSafe: SafeResourceUrl;
-  del: number = 0;
-  addPlaylist: number = 1;
-  url: string = 'http://127.0.0.1:3000/playlistsRetrait/';
-  urlGetVideo: string = 'http://127.0.0.1:3000/getVideoByIdVideo/';
+  del = 0;
+  addPlaylist = 1;
+  url = 'http://127.0.0.1:3000/playlistsRetrait/';
+  urlGetVideo = 'http://127.0.0.1:3000/getVideoByIdVideo/';
+  urlAnnonceAleatoire = 'http://127.0.0.1:3000/annonceur/annoncealeatoire';
   test: any;
   miniature: string;
+  annonce: any;
 
-  constructor(private route: ActivatedRoute, public sanitizer: DomSanitizer, private httpClient: HttpClient, private flashMessage: FlashMessagesService) { }
+  constructor(private route: ActivatedRoute, public sanitizer: DomSanitizer, private httpClient: HttpClient,
+              private flashMessage: FlashMessagesService) {
+  }
 
   ngOnInit(): void {
+    this.getAnnonce();
     this.route.queryParams
       .subscribe(params => {
         this.id = params.id;
         this.idPlaylist = params.idPlaylist;
-        if(params.del==1){
-          this.del=1;
-          this.addPlaylist=0;
-          console.log(params)
+        if (params.del == 1) {
+          this.del = 1;
+          this.addPlaylist = 0;
+          console.log(params);
         }
-      })
+      });
     this.httpClient
-      .get(this.urlGetVideo+this.id)
+      .get(this.urlGetVideo + this.id)
       .subscribe(
         (data) => {
-          this.test=data
-          this.miniature=this.test.miniature
-          this.title=this.test.title
+          this.test = data;
+          this.miniature = this.test.miniature;
+          this.title = this.test.title;
         },
         (error) => {
           console.log('Erreur ! : ' + error);
@@ -49,8 +56,7 @@ export class WatchVideoPlaylistComponent implements OnInit {
       );
 
 
-
-    this.link = "https://www.youtube.com/embed/" + this.id;
+    this.link = 'https://www.youtube.com/embed/' + this.id;
     this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.link);
 
   }
@@ -59,15 +65,32 @@ export class WatchVideoPlaylistComponent implements OnInit {
     this.httpClient
       .patch(this.url + this.idPlaylist, {
         id: this.id
-      }, { responseType: 'text' })
+      }, {responseType: 'text'})
       .subscribe(
         res => {
-          this.flashMessage.show('La vidéo a bien été supprimée de votre playlist.', { cssClass: 'alert-success', timeout: 3000 });
+          this.flashMessage.show('La vidéo a bien été supprimée de votre playlist.', {
+            cssClass: 'alert-success',
+            timeout: 3000
+          });
           this.del = 1;
           this.addPlaylist = 0;
         },
         error => {
           console.log(error);
         });
+  }
+
+  getAnnonce(): void {
+    if (Math.floor(Math.random() * 3) === 0) {
+      this.httpClient.get(this.urlAnnonceAleatoire).subscribe(res => {
+        this.annonce = res;
+        ($('#popupannonce') as any).modal('show');
+      });
+    }
+  } 
+
+  closeAnnonce(): void {
+    ($('#popupannonce') as any).modal('hide');
+
   }
 }
