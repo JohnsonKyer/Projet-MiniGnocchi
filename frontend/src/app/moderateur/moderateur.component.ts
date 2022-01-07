e2cfcc0120d4e1c8e848aab0f088e37de057847e
Cyan <huetcyan@gmail.com>
2022-01-07 01:51:50 +0100

    creation component moderateur

---
+++ frontend/src/app/moderateur/moderateur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-moderateur',
+  templateUrl: './moderateur.component.html',
+  styleUrls: ['./moderateur.component.scss']
+})
+export class ModerateurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
